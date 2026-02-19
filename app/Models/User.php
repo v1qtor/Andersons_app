945e6d58d8465089c9e9e522fe68ad647fa3e97c
RuneLemmens <r0984964@student.thomasmore.be>
2026-02-19 15:26:40 +0100

    added isactive boolean to user

--- app/Models/User.php
+++ app/Models/User.php
@@ -32,6 +32,7 @@ class User extends Authenticatable
         'iban',
         'phoneNumber',
         'countryId',
+        'isActive',
     ];
 
     /**
@@ -54,6 +55,7 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'isActive' => 'boolean',
         ];
     }
 
