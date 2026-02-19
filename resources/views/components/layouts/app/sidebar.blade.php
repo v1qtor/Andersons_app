a933fe8d3654b27be1df48257a4224407c9cc2a4
RuneLemmens <r0984964@student.thomasmore.be>
2026-02-19 15:19:00 +0100

    Added navbar item for manage users page

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -14,6 +14,9 @@
     <flux:navlist variant="outline">
         <flux:navlist.group :heading="__('Platform')" class="grid">
             <flux:navlist.item icon="home" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>{{ __('Dashboard') }}</flux:navlist.item>
+            @if(auth()->user()->role && auth()->user()->role->name === 'Admin')
+                <flux:navlist.item icon="users" :href="route('admin.users.index')" :current="request()->routeIs('admin.users.*')" wire:navigate>{{ __('Users') }}</flux:navlist.item>
+            @endif
         </flux:navlist.group>
     </flux:navlist>
 
