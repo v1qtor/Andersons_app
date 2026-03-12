6b719c101d5f2de6dec8e7ae7c861113fea7fb83
RuneLemmens <r0984964@student.thomasmore.be>
2026-03-12 14:03:07 +0100

    Changed IBAN seeding to be valid IBANS and not faker library "ibans" that are just random strings.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,14 +26,14 @@ class UserFactory extends Factory
     private static int $index = 0;
 
     private static array $users = [
-        ['name' => 'Mr. & Ms. Anderson', 'email' => 'andersons@andersons.com', 'role' => 'The Andersons', 'phone' => '+44 7700 900001'],
-        ['name' => 'Emily Anderson',     'email' => 'emily@andersons.com',     'role' => 'Family Member', 'phone' => '+44 7700 900002'],
-        ['name' => 'James Anderson',     'email' => 'james@andersons.com',     'role' => 'Family Member', 'phone' => '+44 7700 900003'],
-        ['name' => 'Sophie Anderson',    'email' => 'sophie@andersons.com',    'role' => 'Family Member', 'phone' => '+44 7700 900004'],
-        ['name' => 'Tom (Gardener)',     'email' => 'tom.gardener@andersons.com', 'role' => 'Staff',      'phone' => '+44 7700 900005'],
-        ['name' => 'Tom (Handyman)',     'email' => 'tom.handyman@andersons.com', 'role' => 'Staff',      'phone' => '+44 7700 900006'],
-        ['name' => 'Mr. Oliver',         'email' => 'oliver@andersons.com',    'role' => 'Chef',          'phone' => '+44 7700 900007'],
-        ['name' => 'Laurien',            'email' => 'laurien@andersons.com',   'role' => 'Admin',         'phone' => '+44 7700 900008'],
+        ['name' => 'Mr. & Ms. Anderson', 'email' => 'andersons@andersons.com', 'role' => 'The Andersons', 'phone' => '+44 7700 900001', 'iban' => 'GB29 NWBK 6016 1331 9268 19'],
+        ['name' => 'Emily Anderson',     'email' => 'emily@andersons.com',     'role' => 'Family Member', 'phone' => '+44 7700 900002', 'iban' => 'GB82 WEST 1234 5698 7654 32'],
+        ['name' => 'James Anderson',     'email' => 'james@andersons.com',     'role' => 'Family Member', 'phone' => '+44 7700 900003', 'iban' => 'GB57 BARC 2014 7015 2368 25'],
+        ['name' => 'Sophie Anderson',    'email' => 'sophie@andersons.com',    'role' => 'Family Member', 'phone' => '+44 7700 900004', 'iban' => 'GB15 MIDL 4025 3432 1446 70'],
+        ['name' => 'Tom (Gardener)',     'email' => 'tom.gardener@andersons.com', 'role' => 'Staff',      'phone' => '+44 7700 900005', 'iban' => 'GB76 LOYD 3094 7823 4567 12'],
+        ['name' => 'Tom (Handyman)',     'email' => 'tom.handyman@andersons.com', 'role' => 'Staff',      'phone' => '+44 7700 900006', 'iban' => 'GB34 HBUK 4009 1294 5678 12'],
+        ['name' => 'Mr. Oliver',         'email' => 'oliver@andersons.com',    'role' => 'Chef',          'phone' => '+44 7700 900007', 'iban' => 'GB91 BUKB 2000 5534 5678 90'],
+        ['name' => 'Laurien',            'email' => 'laurien@andersons.com',   'role' => 'Admin',         'phone' => '+44 7700 900008', 'iban' => 'GB47 ABBY 0901 2698 7654 32'],
     ];
 
     public function definition(): array
@@ -50,7 +50,7 @@ class UserFactory extends Factory
             'email' => $user['email'],
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'iban' => fake()->iban(),
+            'iban' => $user['iban'],
             'phone_number' => $user['phone'],
             'country_id' => $country?->id,
             'remember_token' => Str::random(10),
