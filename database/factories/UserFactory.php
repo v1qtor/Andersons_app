9ae83233b1959ccfa253d02ed89e2ea76c2ac336
RuneLemmens <r0984964@student.thomasmore.be>
2026-03-12 13:58:35 +0100

    Changed seeding to seed realistic phone numbers

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,14 +26,14 @@ class UserFactory extends Factory
     private static int $index = 0;
 
     private static array $users = [
-        ['name' => 'Mr. & Ms. Anderson', 'email' => 'andersons@andersons.com', 'role' => 'The Andersons'],
-        ['name' => 'Emily Anderson',   'email' => 'emily@andersons.com',   'role' => 'Family Member'],
-        ['name' => 'James Anderson',   'email' => 'james@andersons.com',   'role' => 'Family Member'],
-        ['name' => 'Sophie Anderson',  'email' => 'sophie@andersons.com',  'role' => 'Family Member'],
-        ['name' => 'Tom (Gardener)',   'email' => 'tom.gardener@andersons.com',   'role' => 'Staff'],
-        ['name' => 'Tom (Handyman)',   'email' => 'tom.handyman@andersons.com',   'role' => 'Staff'],
-        ['name' => 'Mr. Oliver',       'email' => 'oliver@andersons.com',       'role' => 'Chef'],
-        ['name' => 'Laurien',          'email' => 'laurien@andersons.com',          'role' => 'Admin'],
+        ['name' => 'Mr. & Ms. Anderson', 'email' => 'andersons@andersons.com', 'role' => 'The Andersons', 'phone' => '+44 7700 900001'],
+        ['name' => 'Emily Anderson',     'email' => 'emily@andersons.com',     'role' => 'Family Member', 'phone' => '+44 7700 900002'],
+        ['name' => 'James Anderson',     'email' => 'james@andersons.com',     'role' => 'Family Member', 'phone' => '+44 7700 900003'],
+        ['name' => 'Sophie Anderson',    'email' => 'sophie@andersons.com',    'role' => 'Family Member', 'phone' => '+44 7700 900004'],
+        ['name' => 'Tom (Gardener)',     'email' => 'tom.gardener@andersons.com', 'role' => 'Staff',      'phone' => '+44 7700 900005'],
+        ['name' => 'Tom (Handyman)',     'email' => 'tom.handyman@andersons.com', 'role' => 'Staff',      'phone' => '+44 7700 900006'],
+        ['name' => 'Mr. Oliver',         'email' => 'oliver@andersons.com',    'role' => 'Chef',          'phone' => '+44 7700 900007'],
+        ['name' => 'Laurien',            'email' => 'laurien@andersons.com',   'role' => 'Admin',         'phone' => '+44 7700 900008'],
     ];
 
     public function definition(): array
@@ -51,7 +51,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'iban' => fake()->iban(),
-            'phone_number' => fake()->phoneNumber(),
+            'phone_number' => $user['phone'],
             'country_id' => $country?->id,
             'remember_token' => Str::random(10),
         ];
