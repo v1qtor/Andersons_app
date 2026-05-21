8411a61c15d7bc465fea1b7c427cdc9de686458b
RuneLemmens <r0984964@student.thomasmore.be>
2026-05-21 13:26:50 +0200

    removed route to old unavailabilities page.

--- resources/views/components/layouts/app/sidebar.blade.php
+++ resources/views/components/layouts/app/sidebar.blade.php
@@ -19,9 +19,9 @@
                     @if(auth()->user()->role && in_array(auth()->user()->role->name, ['Staff', 'Chef', 'Admin', 'The Andersons']))
                         <flux:navlist.item icon="document-text" :href="route('invoices')" :current="request()->routeIs('invoices*')" wire:navigate>{{ __('Invoices') }}</flux:navlist.item>
                     @endif
-                    <!-- @if(auth()->user()->role && in_array(auth()->user()->role->name, ['Staff', 'Chef', 'Admin']))
+                    {{-- @if(auth()->user()->role && in_array(auth()->user()->role->name, ['Staff', 'Chef', 'Admin']))
                         <flux:navlist.item icon="no-symbol" :href="route('unavailabilities')" :current="request()->routeIs('unavailabilities')" wire:navigate>{{ __('Unavailabilities') }}</flux:navlist.item>
-                    @endif -->
+                    @endif --}}
                     @if(auth()->user()->role && in_array(auth()->user()->role->name, ['Staff', 'Chef', 'Family Member', 'The Andersons']))
                         <flux:navlist.item icon="no-symbol" :href="route('unavailability')" :current="request()->routeIs('unavailability')" wire:navigate>{{ __('My Unavailability') }}</flux:navlist.item>
                     @endif
